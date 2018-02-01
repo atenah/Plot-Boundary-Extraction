4b50af99e2534f69eb5400b1df1b67820e030053
Peter Rosario <PeterRosario@ksu.edu>
2018-02-01 15:43:03 -0600

    Added XML configuration file and DTD.

--- lib/validate/validateConfig3.bash
+++ lib/validate/validateConfig3.bash
@@ -4,11 +4,12 @@
 #
 # Validate Config3.xml.
 #
-# Input: lib/Config3.xml, lib/validate/Config3.dtd, and
-#        lib/validate/Config3.xsd.
+# Input: lib/Config3.xml, lib/validate/csv2kml.dtd, and
+#        lib/validate/csv2kml.xsd.
 #
 # Output: If errors are present then they are output.  If no errors then a suc-
 #         cess message is output.
 
 builtin declare -r selfDir="${0%/*}"                # delete shortest end match
-command "${selfDir}/validate.bash" "${selfDir}/../Config3.xml"
+command "${selfDir}/validate.bash" "${selfDir}/../Config3.xml" \
+  "${selfDir}/csv2kml"
